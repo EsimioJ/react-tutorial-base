8167d0993f4dbe5ce5000fa6ee49afbbd8765ee1
Jacopo Riccioni <EsimioJ@users.noreply.github.com>
2022-12-09 15:50:56 +0100

    first

--- appuntamenti/inizio/src/App.js
+++ appuntamenti/inizio/src/App.js
@@ -1,10 +1,18 @@
-import List from "./List";
+import List, { Person } from "./List";
 import data from "./data";
+import { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  //console.log(people);
   return (
-    <div className="App">
-      <h2> App</h2>
+    <div>
+      <h2>INCONTRI</h2>
+      <List data={people} />
+      <div className="btn-group">
+        <button>Reload</button>
+        <button>Delete All</button>
+      </div>
     </div>
   );
 }
--- appuntamenti/inizio/src/List.js
+++ appuntamenti/inizio/src/List.js
@@ -1,11 +1,31 @@
 import React from "react";
 
-const List = () => {
-  return <ul></ul>;
+const List = (props) => {
+  console.log(props);
+  return (
+    <ul>
+      {props.data.map((tipo) => {
+        return (
+          <li key={tipo.id}>
+            <Person {...tipo} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
-const Person = () => {
-  return <article>perosna</article>;
+const Person = ({ id, stato, nome, img }) => {
+  return (
+    <article>
+      <img src={img} alt={nome} className="person-img" />
+      <div>
+        <h2>{nome}</h2>
+        <button>Remove</button>
+      </div>
+      <p>{stato}</p>
+    </article>
+  );
 };
 
 export default List;
